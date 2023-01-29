4d53ee7da33d60ef6939549b62a81fd1ddeb103c
PranshiGarg9420 <75732814+PranshiGarg9420@users.noreply.github.com>
2023-01-29 20:20:13 +0530

    Update pro_237.py

--- pro_237.py
+++ pro_237.py
@@ -1,16 +1,17 @@
-# from plyer import notification
-import time
+with open("file.py","w+") as f:
+    f.write(
+        """
+        import time
+    
+        print("What should I remin you about?")
+        text= str(input())
+        print("In how many minutes?")
+        local_time= float(input())
+        local_time= local_time*60
+        time.sleep(local_time)
+        print(text)
+        
+        """
+    )
+    
 
-
-print("What should I remin you about?")
-text= str(input())
-print("In how many minutes?")
-local_time= float(input())
-local_time= local_time*60
-time.sleep(local_time)
-print(text)
-
-#notification.notify(title="BREAK TIME", message="Time to drink some Water!", timeout=1)
-# while True:
-#     reminder()
-#     time.sleep(10)
